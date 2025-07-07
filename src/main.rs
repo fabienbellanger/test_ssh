8bd594d5714c3d36cf6e747f1fb9f480ba880ef8
Fabien BELLANGER <valentil@gmail.com>
2025-07-07 23:07:15 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 use ssh2::Session;
-use std::{io::Read, net::TcpStream, path::Path};
+use std::{error::Error, io::Read, net::TcpStream, path::Path};
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     // Connect to the local SSH server
-    let tcp = TcpStream::connect("51.15.160.20:22").unwrap();
-    let mut sess = Session::new().unwrap();
+    let tcp = TcpStream::connect("51.15.160.20:22")?;
+    let mut sess = Session::new()?;
     sess.set_tcp_stream(tcp);
-    sess.handshake().unwrap();
+    sess.handshake()?;
 
-    let passphrase =
-        rpassword::prompt_password("Your passphrase: ").expect("Unable to read the password");
+    let passphrase = rpassword::prompt_password("Your passphrase: ")?;
     let passphrase = passphrase.trim();
 
     // Try to authenticate with the first identity in the agent.
@@ -18,21 +17,19 @@ fn main() {
         Some(&Path::new("/Users/fabien/.ssh/id_ed25519.pub")),
         &Path::new("/Users/fabien/.ssh/id_ed25519"),
         Some(passphrase),
-    )
-    .unwrap();
+    )?;
 
     // Make sure we succeeded
     assert!(sess.authenticated());
 
-    println!("Connection success!");
-
     // Try to run the ls command
-    println!("\nRun ls command...");
-    let mut channel = sess.channel_session().unwrap();
-    channel.exec("ls -lha").unwrap();
+    let mut channel = sess.channel_session()?;
+    channel.exec("ls -h")?;
     let mut s = String::new();
-    channel.read_to_string(&mut s).unwrap();
-    println!("ls result:\n{}", s);
-    channel.wait_close().unwrap();
-    println!("--------\nExit status: {}", channel.exit_status().unwrap());
+    channel.read_to_string(&mut s)?;
+    println!("{}", s.trim());
+    channel.wait_close()?;
+    println!("--------\nExit status: {}", channel.exit_status()?);
+
+    Ok(())
 }
